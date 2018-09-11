fa94e02113d9520759c9a4e4581bf4088fe4c216
Artyom <artyomkholodkov@macbook-pro-artyom.local>
2018-09-11 14:14:51 +0200

    Convert to MVC design pattern

---
+++ Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Artyom Kholodkov on 9/11/18.
+//  Copyright © 2018 Artyom Kholodkov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
